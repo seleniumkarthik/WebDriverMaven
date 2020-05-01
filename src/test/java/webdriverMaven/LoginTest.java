e8a3eb12bc99ee970c29421cd0c1aecccf953151
Your Name <you@example.com>
2020-05-01 17:30:50 +0530

    Initial commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,38 @@
+package webdriverMaven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	public static WebDriver driver;
+	@BeforeSuite
+	public void setUp() {
+		
+		System.setProperty("webdriver.gecko.driver", "D:\\GitWebDriverMaven\\geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+	@Test
+	public void doLogin() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("seleniumpupil@gmail.com");
+		driver.findElement(By.xpath("//span[contains(@class,'RveJvd snByac')]")).click();
+		WebDriverWait wait = new WebDriverWait(driver,30); 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='password']")));
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("dssdff");
+		
+	}
+	@AfterSuite
+	public void tearDown() {
+		//driver.quit();
+	}
+
+}
